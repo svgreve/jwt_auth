f758159c58667ca54d5fa3844c895c9e6b54e7e9
Sergio Vieira Greve <sgreve@eikon.com.br>
2021-04-10 13:58:21 -0300

    Guardian config

--- lib/jwt_auth_web/auth/guardian.ex
+++ lib/jwt_auth_web/auth/guardian.ex
@@ -1,14 +1,14 @@
 defmodule JwtAuthWeb.Guardian do
   use Guardian, otp_app: :jwt_auth
 
-  alias JwtAuth
   alias JwtAuth.Accounts.User
+  alias JwtAuth.Accounts.Users.Get, as: UserGet
 
   def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
 
   def resource_from_claims(claims) do
     claims
     |> Map.get("sub")
-
+    |> UserGet.by_id()
   end
 end
